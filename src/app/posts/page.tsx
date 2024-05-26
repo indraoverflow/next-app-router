a3d3eb975790d5baeebaece4abceae2f2dfb2a37
Indra Prasetya <indraprasetya.id@gmail.com>
2024-05-26 10:59:00 +0700

    feat(build): finish

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,36 @@
+import ViewUserButton from "../components/Posts/ViewUserButton";
+import CardList from "../components/Posts/CardList";
+import styles from "./postPage.module.css";
+
+const base_url = "https://jsonplaceholder.typicode.com/posts";
+interface Iposts {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const Posts = async () => {
+  const response = await fetch(base_url, {
+    cache: "no-store",
+  });
+  const posts: Iposts[] = await response.json();
+  return (
+    <>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <h1 className="text-fuchsia-500">POSTINGAN PAGE</h1>
+      {posts.map((post) => {
+        return (
+          <CardList key={post.id}>
+            <p>{post.id}</p>
+            <i>{post.title}</i>
+            <p>{post.body}</p>
+            <ViewUserButton userId={post.userId} />
+          </CardList>
+        );
+      })}
+    </>
+  );
+};
+
+export default Posts;
